3c5c5de00b3373d317260f1393e60370c9401646
Robert Varela <rvarela@utexas.edu>
2016-11-02 17:22:54 -0500

    added more test

--- test/spec/test-spec.js
+++ test/spec/test-spec.js
@@ -13,6 +13,14 @@ describe('home page', function() {
       'Angular OMDb API Search')
   });
 
+  it('Should show the input box on page load', function() {
+    expect(searchInput.isPresent()).toBe(true);
+  });
+
+  it('Should show a submit button', function() {
+    expect(searchButton.isPresent()).toBe(true);
+  });
+
   it('should give results', function() {
     searchInput.sendKeys(
       'Rocky');
@@ -36,6 +44,9 @@ describe('home page', function() {
 
 describe('details page', function() {
 
+  var searchInput = element(by.model('movieTitle')),
+    searchButton = element.all(by.tagName('button')).get(0);
+
   beforeEach(function() {
     browser.get('http://localhost:8000/#/details/tt0075148');
   });
@@ -43,6 +54,20 @@ describe('details page', function() {
   it('should read "Angular OMDb API Search"', function() {
     expect(element.all(by.tagName('h2')).get(0).getText()).toBe(
       'Angular OMDb API Search')
+  });
+
+  it('Should show the input box on page load', function() {
+    expect(searchInput.isPresent()).toBe(true);
+  });
+
+  it('Should show a submit button', function() {
+    expect(searchButton.isPresent()).toBe(true);
+  });
+
+  it('Should show the movie poster', function() {
+    expect(element.all(by.tagName('img')).get(0).getAttribute('src')).toEqual(
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTY5MDMzODUyOF5BMl5BanBnXkFtZTcwMTQ3NTMyNA@@._V1_SX300.jpg"
+    )
   });
 
   it('should show the title', function() {
